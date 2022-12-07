5656645d5ef85f918da208dd3b574300c95944bd
marcLehner <marc.lehner@atos.net>
2022-12-07 14:11:32 +0100

    Test

--- dame_ms2_start/dame/src/com/atos/Main.java
+++ dame_ms2_start/dame/src/com/atos/Main.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
-
+// Hallo Martin, meine Welt!
 //        Scanner scanner = new Scanner(System.in);
 //
 //        String fenString = "1p1p1p1p/p1p1p1p1/1p1p1p1p/8/8/P1P1P1P1/1P1P1P1P/P1P1P1P1 w";
